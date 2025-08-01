0d229c10cbabf224d41018173f0190eddeb535a0
quantus <dw052917@gmail.com>
2025-08-01 21:11:31 +0530

    Update test_data.jl

--- tests/test_data.jl
+++ tests/test_data.jl
@@ -4,4 +4,5 @@ using Collectors
 @testset "Data Tests" begin
     collector = Collectors.init_collector("coingecko")
     @test collector.source == "coingecko"
+    @test haskey(collector.data[], "error") || !isempty(collector.data[])
 end
